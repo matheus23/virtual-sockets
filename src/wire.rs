7083b6f84fa8334fb2fcfbee751c998936644ba3
Philipp Krüger <philipp.krueger1@gmail.com>
2025-05-30 09:35:42 +0200

    Initial commit
    with code extracted from `matheus23/virtual-sockets-2` branch from the iroh-quinn repository.

---
+++ src/wire.rs
@@ -0,0 +1,55 @@
+use tokio::sync::mpsc;
+
+use super::socket::Plug;
+
+pub struct Wire {
+    pub start: Plug,
+    pub end: Plug,
+}
+
+impl Wire {
+    pub fn new(capacity: usize) -> Self {
+        // start -> end transmission
+        let (start_sender, end_receiver) = mpsc::channel(capacity);
+        // end -> start transmission
+        let (end_sender, start_receiver) = mpsc::channel(capacity);
+        let start = Plug {
+            sender: start_sender,
+            receiver: start_receiver,
+        };
+        let end = Plug {
+            sender: end_sender,
+            receiver: end_receiver,
+        };
+
+        Self { start, end }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bytes::Bytes;
+
+    use crate::{TestAddr, socket::VirtualSocket};
+
+    use super::Wire;
+
+    #[tokio::test]
+    async fn test_wire_plugging() -> std::io::Result<()> {
+        let wire = Wire::new(64);
+        let socket0 = VirtualSocket::new(TestAddr(11), wire.start);
+        let mut socket1 = VirtualSocket::new(TestAddr(99), wire.end);
+
+        let contents = Bytes::copy_from_slice(b"Hello, world!");
+        socket0
+            .send_datagram(socket1.addr, contents.clone())
+            .await?;
+
+        let (source_addr, received) = socket1.receive_datagram().await?;
+
+        assert_eq!(source_addr, socket0.addr);
+        assert_eq!(received, contents);
+
+        Ok(())
+    }
+}
---
+++ tests/quinn.rs
@@ -0,0 +1,90 @@
+use std::sync::Arc;
+
+use iroh_quinn::{Endpoint, Runtime as _};
+use iroh_quinn_proto::{ClientConfig, EndpointConfig, ServerConfig};
+use virtual_sockets::{TestAddr, switch::Switch};
+
+#[tokio::test]
+async fn test_connect_with_virtual_socket() {
+    let runtime = Arc::new(iroh_quinn::TokioRuntime);
+
+    // Virtual sockets setup
+    let switch = Switch::new();
+    let server_socket = switch.connect_socket(TestAddr(42)).await;
+    let client_socket = switch.connect_socket(TestAddr(111)).await;
+    let server_addr = server_socket.addr;
+
+    // Cert setup
+    let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
+    let key = rustls::pki_types::PrivateKeyDer::Pkcs8(cert.key_pair.serialize_der().into());
+    let mut roots = rustls::RootCertStore::empty();
+    roots.add(cert.cert.der().clone()).unwrap();
+
+    let server_config = ServerConfig::with_single_cert(vec![cert.cert.der().clone()], key).unwrap();
+    let client_config = ClientConfig::with_root_certificates(Arc::new(roots)).unwrap();
+
+    let server_ep = Endpoint::new_with_abstract_socket(
+        EndpointConfig::default(),
+        Some(server_config),
+        Box::new(server_socket),
+        runtime.clone(),
+    )
+    .unwrap();
+    let mut client_ep = Endpoint::new_with_abstract_socket(
+        EndpointConfig::default(),
+        None,
+        Box::new(client_socket),
+        runtime.clone(),
+    )
+    .unwrap();
+    client_ep.set_default_client_config(client_config);
+
+    // Run server task
+    runtime.spawn(Box::pin({
+        let server_ep = server_ep.clone();
+        async move {
+            // Simple echo loop
+            while let Some(incoming) = server_ep.accept().await {
+                let conn = incoming.accept().unwrap().await.unwrap();
+                conn.close(0u32.into(), b"bye!");
+            }
+        }
+    }));
+
+    // server_socket.set_paused(true);
+    // let mut wiretap = server_socket.wiretap();
+
+    // let (conn, to_resend) = tokio::join!(
+    //     async {
+    // Connect from the client
+    let conn = client_ep
+        .connect(server_addr, "localhost")
+        .unwrap()
+        .await
+        .unwrap();
+    //         conn
+    //     },
+    //     async {
+    //         // Grab the last
+    //         let mut to_resend = wiretap.recv().await.unwrap();
+    //         to_resend.src_ip = SocketAddr::new([192, 168, 0, 133].into(), 1234);
+    //         server_socket
+    //             .try_send(&to_resend.as_quinn_transmit())
+    //             .expect("couldn't resend packet");
+    //         server_socket.set_paused(false);
+    //         to_resend
+    //     }
+    // );
+
+    conn.closed().await;
+
+    client_ep.close(1u32.into(), b"endpoint closed");
+    server_ep.close(1u32.into(), b"endpoint closed");
+
+    // client_socket
+    //     .try_send(&to_resend.as_quinn_transmit())
+    //     .expect("couldn't resend packet");
+
+    client_ep.wait_idle().await;
+    server_ep.wait_idle().await;
+}
